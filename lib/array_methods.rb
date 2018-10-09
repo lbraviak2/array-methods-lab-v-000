4781a1e41519da7e3972f31b742e65cd3af2aa1b
Lindsay Braviak <lbraviak@gmail.com>
2018-10-09 01:07:49 +0000

    automatically backed up by learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,23 @@
 def using_include(array, element)
-
+  array.include?(element)
 end
 
 def using_sort(array)
-
+  arrayTWO = array.sort
 end
 
 def using_reverse(array)
-
+  arrayTWO = array.reverse
 end
 
 def using_first(array)
-
+ FirstItem = array.first
 end
 
 def using_last(array)
-
+  LastItem = array.last
 end
 
 def using_size(array)
-
+  size = array.size
 end
